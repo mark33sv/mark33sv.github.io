73eb387cb9d99b338465fca569c85f4eaf1da08b
mark33sv <markomoran@gmail.com>
2021-07-22 12:42:31 -0600

    subiendo a nuevo repositorio como pagina landing

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Hyperspace by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Bienvenidos</a></li>
+							<li><a href="#one">Conoceme</a></li>
+							<li><a href="#two">Qué hago</a></li>
+							<li><a href="#three">Contactame</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Sr WEb Morán</h1>
+							<p>Aprendiendo Front End - Ricaldone-INSAFORP 2021 <a href="http://html5up.net">HTML5 UP</a><br />
+							and released for free under the <a href="http://html5up.net/license">Creative Commons</a>.</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Learn more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Soy el Sr Web Morán</h2>
+									<p>Aprendiendo en este 2021 el diseño web en modo Front End, para la vista de usuario y flexibilidad del diseño en cuato a móbiles y de escritorio, ampliando técnicas.</p>
+									<ul class="actions">
+										<li><a href="generic.html" class="button">Seguir leyendo más acerca del Front End</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Aprende a CODIFICAR</h2>
+									<p>Seguir Googleando para saber más</p>
+									<ul class="actions">
+										<li><a href="generic.html" class="button">Seguir aprendiendo más...</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Responsive o Diseño Responsivo</h2>
+									<p>Seguir Googleando para saber más</p>
+									<ul class="actions">
+										<li><a href="generic.html" class="button">Seguir aprendiendo más...</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>¿Qué hago en la actualidad?</h2>
+							<p>Estoy aprendiendo más sobre conceptos y técnicas sobre el diseño web y la competitividad en cuanto al uso en móbiles, una mejor forma de presentar diseños y calidad en el mismo para la experiencia de usuarios y sean efectivas para clientes futuros del sitio web</p>
+							<div class="features">
+								<section>
+									<span class="icon solid major fa-code"></span>
+									<h3>¿Qué son los términos de UX / UI ?</h3>
+									<p>Sigue googleando para aprender más sobre estos términos</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-lock"></span>
+									<h3>Plantillas web responsivas</h3>
+									<p>La existencia de miles de plantillas web praa iniciar en el mundo del diseño: desde las palntillas en blanco, conocidas como scratch para iniicar de cero y darle estilos, hasta las ya realizadas y gratuitas que podras solo modificar el código y otros .. como esta plantilla que ves</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-cog"></span>
+									<h3>HTML 5</h3>
+									<p>Sigue googleando para aprender más sobre estos términos</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-desktop"></span>
+									<h3>CSS SASS LASS</h3>
+									<p>Sigue googleando para aprender más sobre estos términos</p>
+								</section>
+								<section>
+									<span class="icon solid major fa-link"></span>
+									<h3>BOOTSTRAP o no BOOTSTRAP?</h3>
+									<p>Sigue googleando para aprender más sobre estos términos</p>
+								</section>
+								<section>
+									<span class="icon major fa-gem"></span>
+									<h3>Cómo ser un Diamante</h3>
+									<p>Sigue googleando para aprender más sobre estos términos</p>
+								</section>
+							</div>
+							<ul class="actions">
+								<li><a href="generic.html" class="button">Aprende más...</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Contáctame ya!</h2>
+							<p>llena el formulario y envíame tu solicitud de diseño web</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="fields">
+											<div class="field half">
+												<label for="name">Nombre</label>
+												<input type="text" name="name" id="name" />
+											</div>
+											<div class="field half">
+												<label for="email">Correo-e</label>
+												<input type="text" name="email" id="email" />
+											</div>
+											<div class="field">
+												<label for="message">Tu mensaje por favor</label>
+												<textarea name="message" id="message" rows="5"></textarea>
+											</div>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Enviar</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Dirección</h3>
+											<span>Calle "El Manguito" Bo. Bosque de Luz<br />
+											San Salvador<br />
+											El Salvador</span>
+										</li>
+										<li>
+											<h3>Correo-e</h3>
+											<a href="#">contacto@srwebmoran.net</a>
+										</li>
+										<li>
+											<h3>Teléfono</h3>
+											<span>(503) 75374914</span>
+										</li>
+										<li>
+											<h3>Sigueme en redes sociales:</h3>
+											<ul class="icons">
+												<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+												<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+												<li><a href="#" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Sr Web Morán. Todos los Derechos Reservados</li><li>Diseñado por: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sr Web Moran</title>
+</head>
+<body>
+	<div class="cabecera">
+		<div class="banner">
+			<img src="#" alt="banner">
+			<img src="#" alt="fb">
+			<img src="#" alt="tw">
+			<img src="#" alt="ins">
+			<img src="#" alt="lkedn">
+			<img src="#" alt="wtsp">
+
+		</div>
+	</div>
+	
+
+</body>
+</html>
